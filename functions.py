1ac7cf5e4d01b317a202abb8f080d53309ff9a81
Gustavo <112530089+Gust4v04lv3s@users.noreply.github.com>
2022-12-09 19:44:42 -0300

    .

---
+++ functions.py
@@ -0,0 +1,133 @@
+from json import load
+from  googletrans import Translator
+
+with open('pokedex.json', encoding='utf8') as file:
+    pokedex151 = load(file)
+
+
+def menu():
+    
+    return ["[ 1 ]  Search by Name",
+        "[ 2 ]  Search by Type",
+        "[ 3 ]  Search by Number",
+        "[ 4 ]  Setting language",
+        "[ 5 ]  Exit"]
+
+def set_langue():
+    print('-'*40)
+    print("What your langue? Default = English ")
+    print("[1] English\n[2] 日本語\n[3] Français")
+    num = input("Input: ").replace(" ", "")
+    num = int(num) 
+    if 1<= num <=3:
+        return str(num)
+    else:
+        num = 1
+        return str(num) 
+
+
+def translate_menu(func, lang="en"):
+    translation = Translator()
+    for items in func():
+        print(translation.translate(items, lang).text)
+
+def translate_word(var, lang):
+    translation = Translator()
+    return (translation.translate(var, lang).text)
+
+
+def  initials_by_name(var):
+    if var == "en":
+        return "english"
+
+    elif var == "ja":
+        return "japanese"
+
+    elif var == "fr":
+        return "french"
+
+
+def num_in_initials(var="1"):
+
+    if var ==  "1":
+        return "en"
+
+    elif var== "2":
+        return "ja"
+
+    elif var== "3":
+        return "fr"
+   
+
+def search_by_name(name, lang, traduction):
+    print('\n','=-'*20,)
+    print("information's\n".center(40))
+
+    if name!="":
+        for group in pokedex151:
+            if name in group['name'].values():
+                print("\t",translate_word("Number", traduction),
+                f"{'':<3}" ,group['id'])
+                
+                if lang != "english":
+                    print("\t",translate_word("Name", traduction), end='')
+                    print(f"{'':<6}", end='')
+                    print(group['name'][lang])
+
+                    print(f"\t {'English ':<10} {group['name']['english']}")
+                else:
+                    print(f"\t {'Name':<10} {group['name'][lang]} ")
+
+                print("\t",translate_word("Type", traduction), end='')
+                print(f"{'':<5}", end='')
+                for items in group['type']:
+                    print(translate_word(items, traduction))
+        return
+    print("Not Found !")
+                
+
+
+def search_by_type(t1, t2):
+    print('\n','-'*40)
+    print("Type".center(40))
+
+    if t1=='' and t2 =='':
+        print("None type selected".center(40))
+        return
+ 
+    accumulator = 0
+    c = 0
+    for pokemon_group in pokedex151:
+        
+        if t2 == '':
+            if t1 in pokemon_group["type"]: 
+                print(f"  // {pokemon_group['name']['english']}", end='// ')
+                accumulator +=1
+                c += 1
+                if c ==  8:
+                    print()
+                    c = 0
+         
+        else:
+            if t1 in pokemon_group.values() and t2 in pokemon_group.values():
+                
+                print(f"  //{pokemon_group['name']}", end='// ')
+                accumulator +=1
+
+    return accumulator
+
+def search_by_id(var,lang):
+    try: 
+        num = int(var)
+        for pokemon_group in pokedex151:
+            if num in pokemon_group.values():
+                print(f"\t{'ID Num':<10} {pokemon_group['id']}")
+                print(f"\t{'Name':<10} {pokemon_group['name'][lang]}")
+                print(f"\t{'Type':<10} ", end='')
+                if len(pokemon_group['type']) > 1:
+                    print(*pokemon_group['type'], sep='// ')
+                else:
+                    print(*pokemon_group['type'])
+    
+    except ValueError:
+        print("\tIt is not number\n")
--- main.py
+++ main.py
@@ -1,131 +1,16 @@
 import os
-from json import load
-from time import sleep
-from  googletrans import Translator
 
-with open('pokedex.json', encoding='utf8') as file:
-    pokedex151 = load(file)
+from functions import menu, set_langue, translate_menu, \
+translate_word, initials_by_name, num_in_initials, \
+search_by_name, search_by_type, search_by_id 
 
-
-def menu():
-    
-    return ["[ 1 ]  Search by Name",
-        "[ 2 ]  Search by Type",
-        "[ 3 ]  Search by Number",
-        "[ 4 ]  Setting langue",
-        "[ 5 ]  Exit"]
-
-def set_langue():
-     print('-'*40)
-     print("What your langue? Default = English ")
-     print("[1] English\n[2] Japanese\n[3] French ")
-     num = input("Input: ")
-     return num
-  
-def translate_menu(func, lang="en"):
-    translation = Translator()
-    for items in func():
-        print(translation.translate(items, lang).text)
 
 
     
-
-def  initials_by_name(var):
-    if var == "en":
-        return "english"
-
-    elif var== "ja":
-        return "japanese"
-
-    elif var== "fr":
-        return "french"
+os.system("cls") 
 
 
-def test_initials_by_name(var="1"):
-    if var ==  "1":
-        return "en"
-
-    elif var== "2":
-        return "ja"
-
-    elif var== "3":
-        return "fr"
-   
-
-def search_by_name(name, lang):
-    print('\n','=-'*20,)
-    print("information's\n".center(40))
-
-    if name!="":
-        for group in pokedex151:
-            if name in group['name'].values():
-                print(f"\t{'ID Num':<10} {group['id']}")
-                if lang != "english":
-                     print(f"\t{'Name':<10} {group['name'][lang]}")
-                     print(f"\t{'English':<10} {group['name']['english']}")
-                else:
-                    print(f"\t{'Name':<10} {group['name'][lang]} ")
-                print(f"\t{'Type':<10}" ,*group['type'], sep=' // ')
-        return
-    print("Not Found !")
-                
-
-
-def search_by_type(t1, t2):
-    print('\n','-'*40)
-    print("Type".center(40))
-
-    if t1=='' and t2 =='':
-        print("None type selected".center(40))
-        return
- 
-    accumulator = 0
-    c = 0
-    for pokemon_group in pokedex151:
-        
-        if t2 == '':
-            if t1 in pokemon_group["type"]: 
-                print(f"  // {pokemon_group['name']['english']}", end='// ')
-                accumulator +=1
-                c += 1
-                if c ==  8:
-                    print()
-                    c = 0
-         
-        else:
-            if t1 in pokemon_group.values() and t2 in pokemon_group.values():
-                
-                print(f"  //{pokemon_group['name']}", end='// ')
-                accumulator +=1
-
-    return accumulator
-
-def search_by_id(var,lang):
-    try: 
-        num = int(var)
-        for pokemon_group in pokedex151:
-            if num in pokemon_group.values():
-                print(f"\t{'ID Num':<10} {pokemon_group['id']}")
-                print(f"\t{'Name':<10} {pokemon_group['name'][lang]}")
-                print(f"\t{'Type':<10} ", end='')
-                if len(pokemon_group['type']) > 1:
-                    print(*pokemon_group['type'], sep='// ')
-                else:
-                    print(*pokemon_group['type'])
-    
-    except ValueError:
-        print("\tIt is not number\n")
-
-def translate_word(var, lang):
-    translation = Translator()
-    return (translation.translate(var,lang).text)
-    
-os.system("cls")  
-
-def return_lang(var):
-    return var
-
-lang = test_initials_by_name()
+lang = num_in_initials() 
 
 while True: 
 
@@ -134,16 +19,16 @@ while True:
     
     lang_in_json = initials_by_name(lang)
 
-    option_ = translate_word("input", lang)
-    option = input(f"{option_}: ")
+    option_ = translate_word("option", lang)
+    option = input(f"{option_}: ").strip()
 
     print("-"*40)
     
-    if option =="1":
+    if option == "1": 
         
-        name = input("What Pokémon?: ")
+        name = input(translate_word("Name of Pokémon: ", lang))
         os.system("cls")
-        search_by_name(name.capitalize().strip(), initials_by_name(lang))
+        search_by_name(name.capitalize().strip(), initials_by_name(lang), lang)
 
     if option == "2":
         type1 = input("what primary type: ").capitalize().strip()
@@ -160,7 +45,7 @@ while True:
     if option == "4":
         os.system("cls")
         lang_set = set_langue()
-        lang = test_initials_by_name(lang_set)
+        lang = num_in_initials(lang_set) 
         os.system("cls")
         
 
